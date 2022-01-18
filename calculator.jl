dcda3b13ebf74abf6ccab0eb10980dc3fff90a10
Logan Kilpatrick <23kilpatrick23@gmail.com>
2022-01-17 17:52:09 -0800

    Update calculator.jl

--- calculator.jl
+++ calculator.jl
@@ -1,15 +1,15 @@
 # Calculator in Julia
 
 function calculator()
-    print("------- Welcome to the Calculator app written in Julia -------")
+    println("------- Welcome to the Calculator app written in Julia -------")
     while true
         # Print the Calculator app Menu|
         println("==== Menu ====")
-        println("1. Addition.")
-        println("2. Subtraction.")
-        println("3  Multiplication.")
-        println("4. Division.")
-        println("5. Exit the Calculator app.")
+        println("1. Addition ➕")
+        println("2. Subtraction ➖")
+        println("3  Multiplication ✖️")
+        println("4. Division ➗")
+        println("5. Exit the Calculator app ↗️")
         operation = parse(Float64, readline())
         # Convert the string value input to a float
         
